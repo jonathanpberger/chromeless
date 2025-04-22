2c0665f70dab93ece813dfd2c28ef705ca9ccecb
JPB <jpb@mac.mynetworksettings.com>
2025-04-22 10:33:26 -0400

    Update Babel configuration to support both modern and legacy presets

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,56 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      targets: {
-        electron: '28',
-        node: '18',
-        browsers: ['>0.2%', 'not dead', 'not op_mini all']
-      },
-      useBuiltIns: 'usage',
-      corejs: 3
-    }],
-    ['@babel/preset-react', {
-      runtime: 'automatic'
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+module.exports = function(api) {
+  // This caches the Babel config
+  api.cache.using(() => process.env.NODE_ENV);
+  
+  const presets = [
+    // For backward compatibility with react-scripts
+    'babel-preset-env',
+    'babel-preset-react',
+    
+    // Modern presets
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          electron: '28',
+          node: '18',
+          browsers: ['>0.2%', 'not dead', 'not op_mini all']
+        },
+        useBuiltIns: 'usage',
+        corejs: 3
+      }
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic'
+      }
+    ]
+  ];
+  
+  const plugins = [
+    ['@babel/plugin-transform-runtime', {
+      regenerator: true
     }]
-  ],
-  env: {
-    test: {
-      presets: [
-        ['@babel/preset-env', {
-          targets: {
-            node: 'current'
-          }
-        }]
-      ]
+  ];
+  
+  return {
+    presets,
+    plugins,
+    env: {
+      test: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              node: 'current'
+            }
+          }]
+        ]
+      }
     }
-  }
+  };
 };
